3bababe7094546f928b06f26eb5ff4e68d073a5c
ADGGG <adamgo060206@gmail.com>
2026-09-24 18:34:56 +0800

    5th repo

---
+++ high score indicator.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        // Initialize your values here
+        int score = 93;
+        int bonus = 17;
+        
+        // Don't modify the code below
+        String status = "Regular Score";
+        
+        if (score >= 80 && score <= 100 && bonus >= 10 && bonus <= 20) {
+            if ((score > 90 && bonus > 15) || (score > 85 && bonus > 18)) {
+                status = "High Score";
+            }
+        } else {
+            status = "Invalid Values";
+        }
+        
+        System.out.println("Status: " + status);
+    }
+}
